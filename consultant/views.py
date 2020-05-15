4707ee669e71c4849151d4e558a19b9e4e3cf6c1
Devesh Nanwani <consultadd@g.com>
2020-05-15 14:07:53 +0530

    feedback updation

--- consultant/views.py
+++ consultant/views.py
@@ -1167,7 +1167,7 @@ class FeedbackViewSet(GenericViewSet, CreateModelMixin, UpdateModelMixin, Retrie
             feedback = Feedback.objects.filter(consultant_id=kwargs.get('pk')).order_by('-created')
             if feedback_type:
                 feedback = feedback.filter(feedback_type=feedback_type)
-            serializer = ConsultantGetFeedbackSerializer(feedback, many=True)
+            serializer = self.serializer_class(feedback, many=True)
             return Response({"result": serializer.data}, status=status.HTTP_200_OK)
         except Exception as error:
             return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
