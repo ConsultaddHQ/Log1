a4d6ee499a5f4f56d1103329326fa318718dcaf0
Sarang Mulewa <sarangmuleva.1@gmail.com>
2020-07-28 08:59:54 +0000

    Hotfixes

--- utils_app/calendar.py
+++ utils_app/calendar.py
@@ -1,7 +1,28 @@
 import os
+import json
+import requests
 import httplib2
 from googleapiclient.discovery import build
 from oauth2client.client import OAuth2Credentials
+
+
+def create_ms_token():
+    tenant_id = os.environ.get('tenant_id')
+    client_id = os.environ.get('client_id')
+    client_secret = os.environ.get('client_secret')
+    scope = 'https%3A//graph.microsoft.com/.default'
+
+    url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
+    payload = f'client_id={client_id}&client_secret={client_secret}&scope={scope}&grant_type=client_credentials'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    data = json.loads(response.text.encode('utf8'))
+    access_token = None
+    if response.status_code == 200:
+        access_token = data["access_token"]
+    return access_token
 
 
 def calendar_con():
@@ -53,6 +74,157 @@ def calendar_description(data):
 
     '''
     return description
+
+
+def calendar_ms_description(data):
+    description = f'''
+
+    <div><Strong>Calling Details</Strong></div> 
+        {data["call_details"]} </br></br>
+
+    <div><Strong>Marketer Name -</Strong> {data["user"].employee_name}</div> 
+    <div><Strong>Employer - </Strong>{data["submission"].employer}</div> </br>
+
+    <div><Strong>consultant Details:</Strong> </div>
+
+       <Strong> Name -</Strong> {data["consultant"].name}  </br>
+       <Strong> DOB - </Strong>{data["submission"].date_of_birth}</br> 
+        <Strong>SSN -</Strong> {data["consultant"].ssn} </br>
+        <Strong>VISA - </Strong>{data["submission"].visa_type}</br> 
+        <Strong>Visa Start -</Strong> {data["submission"].visa_start}</br> 
+        <Strong>Visa End -</Strong>{data["submission"].visa_end}</br>
+
+       <Strong> Skype id </Strong>- {data["consultant"].skype}</br>
+
+        <Strong>Education </Strong>- {data["submission"].education}</br></br>
+
+    <div><Strong>Position Details:</Strong></div>
+
+       <Strong> Location - </Strong>{data["lead"].city}</br>
+       <Strong> Job Title - </Strong>{data["lead"].job_title}</br>
+       <Strong> Client Name - </Strong>{data["submission"].client}</br></br>
+
+    <div><Strong>Extra details:</Strong> </div>
+        {data["description"]}</br></br>
+
+    <div><Strong>Job Description:</Strong></div>
+        {data["lead"].job_desc}</br></br>
+
+    '''
+    return description
+
+
+def book_ms_calendar(data):
+    try:
+        token = create_ms_token()
+        description = calendar_ms_description(data)
+        attendees = []
+        for i in data['attendees']:
+            attendees.append({
+                "EmailAddress": {
+                    "Address": i['email'],
+                },
+            })
+
+        event = json.dumps({
+            "Subject": data["summary"],
+            "Body": {
+                "ContentType": "HTML",
+                "Content": description
+            },
+            "Start": {
+                "DateTime": data["start"],
+                "TimeZone": "Eastern Standard Time"
+            },
+            "End": {
+                "DateTime": data["end"],
+                "TimeZone": "Eastern Standard Time"
+            },
+            "Attendees": attendees
+        })
+        headers = {
+            "Authorization": "bearer " + token,
+            "Content-Type": "application/json"
+        }
+        url = f"https://graph.microsoft.com/v1.0/Users/{os.environ.get('user_id')}/events/"
+        response = requests.post(url, headers=headers, data=event)
+        if response.status_code == 201:
+            return json.loads(response.text.encode('utf-8'))
+        return False
+    except Exception as error:
+        print(error)
+        return False
+
+
+def update_ms_calendar(event_id, data):
+    try:
+        token = create_ms_token()
+        description = calendar_ms_description(data)
+        attendees = []
+        for i in data['attendees']:
+            attendees.append({
+                "EmailAddress": {
+                    "Address": i['email'],
+                },
+            })
+
+        event = json.dumps({
+            "Subject": data["summary"],
+            "Body": {
+                "ContentType": "HTML",
+                "Content": description
+            },
+            "Start": {
+                "DateTime": data["start"],
+                "TimeZone": "Eastern Standard Time"
+            },
+            "End": {
+                "DateTime": data["end"],
+                "TimeZone": "Eastern Standard Time"
+            },
+            "Attendees": attendees
+        })
+        headers = {
+            "Authorization": "bearer " + token,
+            "Content-Type": "application/json"
+        }
+        url = f"https://graph.microsoft.com/v1.0/Users/{os.environ.get('user_id')}/events/{event_id}/"
+        response = requests.patch(url, headers=headers, data=event)
+        if response.status_code == 200:
+            return json.loads(response.text.encode('utf-8'))
+        return False
+    except Exception as error:
+        print(error)
+        return False
+
+
+def delete_ms_calendar(event_id):
+    try:
+        token = create_ms_token()
+        headers = {
+            "Authorization": "bearer " + token,
+            "Content-Type": "application/json"
+        }
+        url = f"https://graph.microsoft.com/v1.0/Users/{os.environ.get('user_id')}/events/{event_id}/"
+        response = requests.delete(url, headers=headers)
+        if response.status_code == 204:
+            return True
+        return False
+    except Exception as error:
+        print(error)
+        return False
+
+
+def get_user_id(email):
+    token = create_ms_token()
+    headers = {
+        "Authorization": "bearer " + token,
+        "Content-Type": "application/json"
+    }
+    url = f"https://graph.microsoft.com/v1.0/users/?$select=displayName,id&$filter=identities/any(c:c/issuerAssignedId eq '{email}')"
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text.encode('utf-8'))
+    return data
 
 
 def book_calendar(data):
