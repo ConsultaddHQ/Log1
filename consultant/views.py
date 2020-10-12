15df1fa424fa7611a6749bf7cd18543d2dda6944
Devesh <deveshnanwani@gmail.com>
2020-10-12 13:58:41 +0530

    attachment delete access to recruiter

--- consultant/views.py
+++ consultant/views.py
@@ -767,7 +767,7 @@ class ConsultantViewSets(viewsets.ModelViewSet):
     def payroll_employer(self, request, *args, **kwargs):
         if request.method == 'GET':
             try:
-                consultant = Consultant.objects.get(id=kwargs.get('pk'))
+                consultant = get_object_or_404(Consultant, id=kwargs.get('pk'))
                 serializer = PayrollEmployerSerializer(consultant.employers.all().order_by('-start'), many=True)
                 return Response({"results": serializer.data}, status=status.HTTP_200_OK)
             except Exception as error:
@@ -775,21 +775,18 @@ class ConsultantViewSets(viewsets.ModelViewSet):
         elif request.method == 'PUT':
             try:
                 employer = PayrollEmployer.objects.get(id=kwargs.get('pk'))
-                employer.start = request.data['employer_start_date']
-                employer.save()
-                serializer = PayrollEmployerSerializer(employer)
+                serializer = PayrollEmployerSerializer(employer, data=request.data, partial=True)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
                 return Response({"results": serializer.data}, status=status.HTTP_202_ACCEPTED)
             except Exception as error:
                 return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
         else:
             try:
-                consultant = Consultant.objects.get(id=kwargs.get('pk'))
-                employer = PayrollEmployer.objects.create(
-                    consultant=consultant,
-                    name=request.data['payroll_employer'],
-                    start=request.data['employer_start_date'],
-                )
-                serializer = PayrollEmployerSerializer(employer)
+                consultant = get_object_or_404(Consultant, id=kwargs.get('pk'))
+                serializer = PayrollEmployerSerializer(data=request.data, partial=True)
+                serializer.is_valid(raise_exception=True)
+                serializer.save(consultant=consultant)
                 return Response({"results": serializer.data}, status=status.HTTP_201_CREATED)
             except Exception as error:
                 return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
