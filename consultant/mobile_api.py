5088abd5bd4b4dccaa50422be4658077a6b9ae92
Sarang Mulewa <sarang.m@consultadd.com>
2021-08-05 13:08:04 +0530

    improvements

--- consultant/mobile_api.py
+++ consultant/mobile_api.py
@@ -17,8 +17,8 @@ from utils_app.mailing import send_email
 from log1.utils import write_exception, write_info
 from consultant.permissions import ConsultantIsAuthenticated
 from consultant.serializers import ConsultantLoginSerializer
+from employee.models import clear_expired, get_token_expiry_time
 from employee.serializers import EmailSerializer, PasswordTokenSerializer
-from employee.models import clear_expired, get_password_reset_token_expiry_time
 from consultant.authentication import consultant_authenticate, ConsultantTokenAuthentication
 from consultant.models import Consultant, ConsultantToken, ConsultantResetPasswordToken, FCMDevice
 
@@ -194,7 +194,7 @@ class ConsultantResetPasswordViewSet(GenericViewSet):
         serializer.is_valid(raise_exception=True)
         email = serializer.validated_data['email']
 
-        password_reset_token_validation_time = get_password_reset_token_expiry_time()
+        password_reset_token_validation_time = get_token_expiry_time()
 
         now_minus_expiry_time = timezone.now() - timedelta(hours=password_reset_token_validation_time)
 
@@ -252,7 +252,7 @@ class ConsultantResetPasswordViewSet(GenericViewSet):
         password = serializer.validated_data['password']
         token = serializer.validated_data['token']
 
-        password_reset_token_validation_time = get_password_reset_token_expiry_time()
+        password_reset_token_validation_time = get_token_expiry_time()
 
         reset_password_token = ConsultantResetPasswordToken.objects.filter(key=token).first()
 
