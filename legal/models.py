927a1196fcc8b8829acedbce5a0ead273a8a205b
Shreyas <shreyas.k@consultadd.com>
2022-02-10 13:15:31 +0530

    add is_withdraw key to petition model.

--- legal/models.py
+++ legal/models.py
@@ -29,7 +29,6 @@ PETITION_STATUSES = (
     ('approved', 'Approved'),
     ('assigned', 'Assigned'),
     ('reviewed', 'Reviewed'),
-    ('withdrawn', 'Withdrawn'),
     ('lca_filed', 'LCA Filed'),
     ('print', 'Sent for Print'),
     ('lca_approved', 'LCA Approved'),
@@ -64,6 +63,7 @@ class Types(models.Model):
 class Petition(TimeStampedModel):
     comments = GenericRelation(Comment, verbose_name="comments")
     is_active = models.BooleanField(_('Is Petition Active'), default=True)
+    is_withdraw = models.BooleanField(_('Withdraw Petition'), default=False)
     expiry_date = models.DateField(_('Expiry Date'), null=True, default=None)
     lca_no = models.CharField(_('LCA No.'), max_length=40, null=True, blank=True)
     employer = models.CharField(_('Employer'), max_length=20, default='Consultadd')
--- legal/serializers.py
+++ legal/serializers.py
@@ -113,7 +113,7 @@ class PetitionGetSerializer(serializers.ModelSerializer):
         model = Petition
         fields = ('id', 'petition_type', 'employer', 'consultant', 'assigned_to', 'beneficiary_type', 'docs', 'reasons',
                   'status', 'lca_no', 'uscis_no', 'fedex_no', 'premium_processing', 'created_by', 'is_active', 'rfe',
-                  'expiry_date')
+                  'expiry_date', 'is_withdraw')
 
     @staticmethod
     def get_rfe(obj):
