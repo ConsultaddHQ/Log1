50e92dab751c0fb705d5124a1066342fd8cd0613
Shreyas <shreyas.k@consultadd.com>
2022-04-04 16:14:48 +0530

    update from dev

--- engineering/serializers.py
+++ engineering/serializers.py
@@ -415,7 +415,6 @@ class EngineerTestSerializer(serializers.ModelSerializer):
         return {
             "id": submission.id,
             "client": submission.client,
-            "submission_id": submission.id,
             "job_title": submission.lead.job_title,
             "marketer_name": submission.created_by.employee_name,
             "vendor_company": submission.lead.vendor_company.name,
@@ -451,7 +450,6 @@ class EngineerInterviewSerializer(serializers.ModelSerializer):
         return {
             "id": submission.id,
             "client": submission.client,
-            "submission_id": submission.id,
             "job_title": submission.lead.job_title,
             "marketer_name": submission.created_by.employee_name,
             "vendor_company": submission.lead.vendor_company.name,
