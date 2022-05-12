7ba4800d088172f895b75746d1c9e0eee078e0e2
Ubuntu <ubuntu@ip-172-31-3-32.ap-south-1.compute.internal>
2022-05-12 05:52:23 +0000

    resolve merge conflict

--- project/mobile_api.py
+++ project/mobile_api.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
@@ -16,13 +16,15 @@ from constance import config
 from employee.models import User
 from log1.utils import write_exception
 from attachment.models import Attachment
+from consultant.models import Consultant
 from utils_app.mailing import send_email
 from utils_app.aws_utils import get_s3_object
 from consultant.permissions import ConsultantIsAuthenticated
 from consultant.authentication import ConsultantTokenAuthentication
 from notification.utils import create_notification, push_notification
-from project.models import Project, TimeSheet, PayrollSchedule, ProjectStatus
-from project.serializers import TimeSheetSerializer, PayrollScheduleSerializer, ProjectTimeSheetSerializer
+from project.models import Project, TimeSheet, PayrollSchedule, ProjectStatus, ConsultantLeave, Leave
+from project.serializers import TimeSheetSerializer, PayrollScheduleSerializer, ProjectTimeSheetSerializer, \
+    ConsultantLeaveSerializer, LeaveSerializer
 
 
 # Route - /payroll/
@@ -607,6 +609,91 @@ class TimeSheetViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, Updat
                     "created": attachment.created,
                     "file_name": attachment.filename,
                 })
+
+            return Response({"result": data}, status=200)
+        except Exception as error:
+            write_exception(error, request)
+            return Response({"error": str(error)}, status=400)
+
+
+# Route - /consultant_leave/
+class ConsultantLeaveViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin):
+    queryset = ConsultantLeave.objects.all()
+    serializer_class = ConsultantLeaveSerializer
+    permission_classes = (ConsultantIsAuthenticated,)
+    authentication_classes = (ConsultantTokenAuthentication,)
+
+    @action(methods=['GET'], detail=True, url_path='balance')
+    def balance(self, request, pk):
+        try:
+            year = date.today().year
+            leaves = ConsultantLeave.objects.filter(consultant_id=pk, year=year, is_expired=False)
+            serial = ConsultantLeaveSerializer(leaves, many=True)
+            return Response({"result": serial.data}, status=200)
+        except Exception as error:
+            write_exception(error, request)
+            return Response({"error": str(error)}, status=400)
+
+    @action(methods=['POST'], detail=True, url_path='apply')
+    def apply(self, request, pk, *args, **kwargs):
+        try:
+            data = request.data
+            leave_type = get_object_or_404(ConsultantLeave, id=data.get('leave_type'), is_expired=False)
+            leave = Leave.objects.create(
+                leave_type=leave_type,
+                applied_on=date.today(),
+                to_date=data.get('to_date'),
+                from_date=data.get('from_date'),
+                description=data.get('description', None),
+            )
+
+            if data['duration_type'] == 'hourly':
+                leave.total_hours = float(data.get("hours"))
+            elif data['duration_type'] == 'half':
+                leave.total_hours = 4
+            elif data['duration_type'] == 'full':
+                leave.total_hours = 8
+            else:
+                days = datetime.strptime(leave.to_date, "%Y-%m-%d") - datetime.strptime(leave.from_date, "%Y-%m-%d")
+                leave.total_hours = (days.days + 1)*8
+            leave.status = 'availed'
+            leave.save()
+            leave_type.balance = leave_type.balance - leave.total_hours
+            leave_type.save()
+
+            content_type = ContentType.objects.get(model='leave')
+            if request.FILES.get('attachment', None):
+                Attachment.objects.create(
+                    creator_id=1,
+                    object_id=leave.id,
+                    content_type=content_type,
+                    attachment_type='consultant_leave',
+                    attachment_file=request.FILES.get('attachment'),
+                )
+
+            return Response({"message": "leave applied successfully"}, status=201)
+        except Exception as error:
+            write_exception(error, request)
+            return Response({"error": str(error)}, status=400)
+
+    @action(methods=['GET'], detail=True, url_path='history')
+    def history(self, request, pk):
+        try:
+            consultant = get_object_or_404(Consultant, id=pk)
+            leaves = Leave.objects.filter(leave_type__consultant=consultant, leave_type__is_expired=False)
+            serial = LeaveSerializer(leaves, many=True)
+            return Response({"result": serial.data}, status=200)
+        except Exception as error:
+            write_exception(error, request)
+            return Response({"error": str(error)}, status=400)
+
+    @action(methods=['GET'], detail=True, url_path='type')
+    def type(self, request, pk):
+        try:
+            data = []
+            leaves = ConsultantLeave.objects.filter(consultant_id=pk, is_expired=False)
+            for leave in leaves:
+                data.append({"id": leave.id, "leave_type": leave.leave_type.display_name, "balance": leave.balance})
 
             return Response({"result": data}, status=200)
         except Exception as error:
