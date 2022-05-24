058c848cf264146de50559b134bf8ac75c36163a
suman mandal <suman.m@consultadd.com>
2022-05-24 12:48:57 +0530

    remove end date for green card visa type

--- consultant/views.py
+++ consultant/views.py
@@ -1314,17 +1314,27 @@ class WorkAuthViewSets(CreateModelMixin, UpdateModelMixin, GenericViewSet):
                 previous_work_auth = instance.first()
                 previous_work_auth.is_current = False
                 previous_work_auth.save()
-            work_auth = WorkAuth.objects.create(
-                is_current=True,
-                visa_end=request.data['visa_end'],
-                visa_type=request.data['visa_type'],
-                visa_start=request.data['visa_start'],
-                consultant_id=request.data['consultant'],
-            )
+            if request.data['visa_type'] == 'gc':
+                work_auth = WorkAuth.objects.create(
+                    is_current=True,
+                    visa_type=request.data['visa_type'],
+                    visa_start=request.data['visa_start'],
+                    consultant_id=request.data['consultant'],
+                )
+            else:
+                work_auth = WorkAuth.objects.create(
+                    is_current=True,
+                    visa_end=request.data['visa_end'],
+                    visa_type=request.data['visa_type'],
+                    visa_start=request.data['visa_start'],
+                    consultant_id=request.data['consultant'],
+                )
             profiles = work_auth.consultant.profiles.filter(title__iexact='Original')
             if profiles:
                 profile = profiles.first()
-                profile.visa_end = work_auth.visa_end
+                if request.data['visa_type'] != 'gc':
+                    profile.visa_end = work_auth.visa_end
+
                 profile.visa_type = work_auth.visa_type
                 profile.visa_start = work_auth.visa_start
                 profile.save()
