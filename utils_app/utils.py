806b4e37f01896137a54e9d07f14d24d159fec5e
Shreyas  Khede <shreyas.k@consultadd.com>
2022-06-03 09:38:10 +0000

    consultant/Log1-53_62_61

--- utils_app/utils.py
+++ utils_app/utils.py
@@ -46,10 +46,14 @@ def create_cron_object(name):
 
 
 def get_timezone(city_name):
-    obj = TimezoneFinder()
-    geo_locator = Nominatim(user_agent="geoapiExercises")
-    location = geo_locator.geocode(city_name)
-    result = obj.timezone_at(lat=location.latitude, lng=location.longitude)
-    today = datetime.now(tz=timezone(result))
-    return today.strftime("%Z")
+    try:
+        obj = TimezoneFinder()
+        geo_locator = Nominatim(user_agent="geoapiExercises")
+        location = geo_locator.geocode(city_name)
+        result = obj.timezone_at(lat=location.latitude, lng=location.longitude)
+        today = datetime.now(tz=timezone(result))
+        return today.strftime("%Z")
+    except Exception as error:
+        write_exception(message=error)
+        return None
 
