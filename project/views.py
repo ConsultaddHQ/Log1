474592337923989cd4b5bc39fecdcc031282f843
Shreyas  Khede <shreyas.k@consultadd.com>
2022-11-09 09:24:29 +0000

    Staging

--- project/views.py
+++ project/views.py
@@ -196,9 +196,9 @@ class ProjectViewSets(ModelViewSet):
         support_res, support_msg = self.send_support_mail(project, scrum_masters, request)
         engineer = get_object_or_404(User, employee_id=request.data['engineer']) \
             if request.data.get('engineer', None) else None
-        if engineer:
-            support = get_object_or_404(ProjectSupport, project=project, support=engineer)
-            support_assignment_mail(support, request)
+        # if engineer:
+        #     support = get_object_or_404(ProjectSupport, project=project, support=engineer)
+        #     support_assignment_mail(support, request)
         message = "Project created"
         exception_msg = "Mail sent"
         if support_msg == 'error' and offer_msg == 'error':
@@ -527,7 +527,8 @@ class ProjectViewSets(ModelViewSet):
                 {"name": "duration", "display_name": "Duration"},
                 {"name": "city", "display_name": "City"},
                 {"name": "is_remote", "display_name": "Remote"},
-                {"name": "status", "display_name": "Status"}
+                {"name": "status", "display_name": "Status"},
+                {"name": "rate", "display_name": "Rate"}
             ]
             if export:
                 url = export_to_csv(
@@ -1346,6 +1347,67 @@ class FinanceTimeSheetViewSets(RetrieveModelMixin, ListModelMixin, UpdateModelMi
             if query:
                 query = query.lstrip().replace(':amp:', '&')
                 consultants = Consultant.objects.filter(
+                    Q(name__istartswith=query) |
+                    Q(projects__employer__startswith=query) |
+                    Q(projects__submission__client__icontains=query) |
+                    Q(projects__submission__lead__vendor_company__name__icontains=query)
+                ).order_by('id').distinct('id')
+
+            queryset = consultants.order_by('name').distinct('name')
+            total = queryset.count()
+            serializer = ConsultantTimeSheetSerializer(queryset[first:last], many=True)
+            return Response({"data": serializer.data, 'total': total}, status=200)
+        except Exception as error:
+            write_exception(error, request)
+            return Response({"message": ERROR_MSG, "error": str(error)}, status=400)
+
+    @action(methods=["get"], detail=False, url_name="consultant")
+    def consultant(self, request, *args, **kwargs):
+        first, last = get_page_limits(request)
+        query = request.GET.get('query', None)
+        leave_status = request.GET.get('leave_status', '')
+        consultant_id = request.GET.get('consultant', None)
+        consultant_name = request.GET.get('consultant_name', None)
+        timesheet_status = request.GET.get('timesheet_status', [])
+
+        try:
+            project_status = [
+                'terminated-fired_performance_issue', 'terminated-fired_security_issue',
+                'terminated-resigned_full_time_offer', 'terminated-resigned_technology_issue',
+                'terminated-fired_budget_issue', 'terminated-resigned_location_issue', 'complete',
+                'joined', 'terminated', 'terminated-resigned', 'terminated-resigned_location_issue',
+            ]
+
+            if consultant_id:
+                consultants = Consultant.objects.filter(id=consultant_id)
+            elif consultant_name:
+                consultants = Consultant.objects.filter(name__istartswith=consultant_name)
+            else:
+                consultant_ids = Project.objects.filter(
+                    statuses__status__in=project_status, statuses__is_current=True
+                ).values_list('consultant', flat=True)
+
+                consultants = Consultant.objects.filter(
+                    id__in=list(consultant_ids),
+                ).exclude(projects__submission__status='archive').order_by('id').distinct('id')
+
+            if timesheet_status:
+                if timesheet_status == 'pending_for_approval':
+                    consultants = consultants.filter(
+                        projects__timesheets__status__in=['submitted', 'updated'], projects__timesheets__is_active=True
+                    )
+                else:
+                    consultants = consultants.filter(
+                        projects__timesheets__status=timesheet_status, projects__timesheets__is_active=True
+                    )
+            if leave_status:
+                if leave_status == 'pending':
+                    consultants = consultants.filter(leaves__status='applied')
+                elif leave_status == 'not_pending':
+                    consultants = consultants.exclude(leaves__status='applied')
+            if query:
+                query = query.lstrip().replace(':amp:', '&')
+                consultants = consultants.filter(
                     Q(name__istartswith=query) |
                     Q(projects__employer__startswith=query) |
                     Q(projects__submission__client__icontains=query) |
--- utils_app/management/commands/add_technology.py
+++ utils_app/management/commands/add_technology.py
@@ -1,6 +1,8 @@
+import csv
 from django.core.management import BaseCommand
 
 from consultant.models import Consultant
+from employee.models import Certificate
 from utils_app.utils import create_cron_error, create_cron_object, get_timezone
 from utils_app.models import Choice, ContentType
 
@@ -9,10 +11,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         job = create_cron_object(name='add_technology')
         try:
-            TECHNOLOGIES = ['Python', 'Java', 'Nodejs', 'JavaScript', 'ReactJS', 'Angular', 'SQL', 'AWS', 'DevOps',
-                            'BA', 'DA', 'Peoplesoft', 'Workday', 'Kronos', 'Lawson', 'Full Stack', 'Salesforce', 'Cyber Security']
-            content_type = ContentType.objects.get(model='user')
-            for technology in TECHNOLOGIES:
-                chn = Choice.objects.create(name=technology, display_name=technology, content_type=content_type, field='technology')
+            i = 0
+            file = open('certification_list.csv', 'r')
+            data = csv.reader(file)
+            for item in data:
+                if i == 0:
+                    i += 1
+                    continue
+                Certificate.objects.create(name=item[0], issued_by=item[1])
+                i += 1
         except Exception as error:
             print(error)
