cd32258eb3a77c160cb04afd32fabbf043748c78
Shreyas  Khede <shreyas.k@consultadd.com>
2023-02-15 09:40:33 +0000

    Staging

--- utils_app/management/commands/rate_due_notification.py
+++ utils_app/management/commands/rate_due_notification.py
@@ -1,14 +1,14 @@
 import os
 
-from django.db.models import Q
+import csv
 from datetime import date, timedelta
 from django.core.management import BaseCommand
 
+from employee.models import User
 from project.models import Project
-from log1.utils import write_exception
-from utils_app.mailing import send_email
-from consultant.models import Consultant, ConsultantRateRevision
-from utils_app.utils import create_cron_error, create_cron_object
+from utils_app.mailing import send_email_attachment_multiple
+from consultant.models import Consultant, ConsultantRateRevision, ConsultantPOC
+from utils_app.utils import create_cron_error, create_cron_object, delete_temp_file
 
 
 class Command(BaseCommand):
@@ -20,54 +20,83 @@ class Command(BaseCommand):
             data = []
             counter = 1
             consultants = Consultant.objects.filter(status__in=['on_project'])
+            file = open('consultant_rate_revision.csv', 'w+')
+            writer = csv.writer(file)
+            writer.writerow(
+                ['Consultant Name', 'Marketer Name', "Project rate", 'Consultant rate', 'Margin', 'Last Revised Date',
+                 'Vendor Name']
+            )
             for consultant in consultants:
-                try:
-                    rate_qs = ConsultantRateRevision.objects.filter(consultant_id=consultant.id, end=None)
-                    if rate_qs:
-                        con_rate = rate_qs.first().rate
-                        revision_date = rate_qs.first().start
-                    else:
-                        con_rate = 0
-                        revision_date = date(2010, 1, 1)
-                    projects = Project.objects.filter(
-                        (
-                                Q(consultant_id=consultant.id) |
-                                Q(submission__consultant_marketing__consultant_id=consultant.id)
-                        ) & (
-                            Q(statuses__status='joined')
-                        )
-                    )
-                    project_rate = 0
-                    for project in projects:
-                        project_rate = project.rate
-                        if revision_date < project.start_date:
-                            revision_date = project.start_date
-                    if date.today() - timedelta(days=170) < revision_date:
-                        data.append({
-                            "count": counter,
-                            "rate": con_rate,
-                            "consultant_id": consultant.id,
-                            "consultant_name": consultant.name,
-                            "consultant_email": consultant.email,
-                            "margin": project_rate - con_rate if project_rate != 0 else None,
-                        })
-                        counter += 1
-                except Exception as error:
-                    write_exception(message=error)
-
+                last_revision = ConsultantRateRevision.objects.filter(consultant_id=consultant.id, end=None).first()
+                if last_revision:
+                    consultant_rate = last_revision.rate
+                    revision_date = last_revision.start
+                else:
+                    consultant_rate = 0
+                    revision_date = date(2010, 1, 1)
+                project = Project.objects.filter(
+                    is_remote=False, statuses__status='joined', statuses__is_current=True
+                ).select_related('submission').filter(submission__consultant_marketing__consultant_id=consultant.id,
+                                                      ).order_by('-rate').first()
+                if not project:
+                    continue
+                project_rate = project.rate
+                if revision_date < project.start_date:
+                    revision_date = project.start_date
+                margin = project_rate - consultant_rate
+                margin_percentage = round((margin / project_rate) * 100, 2)
+                marketer = {}
+                assigned_marketer = ConsultantPOC.objects.filter(
+                    poc_type='marketer', consultant_id=consultant.id, end=None).first()
+                if not assigned_marketer:
+                    marketer['name'] = project.submission.created_by.employee_name
+                    marketer['email'] = project.submission.created_by.email
+                else:
+                    marketer['name'] = assigned_marketer.poc.employee_name
+                    marketer['email'] = assigned_marketer.poc.email
+                if (date.today() - timedelta(days=150) < revision_date) and margin_percentage < 30:
+                    consultant_info = {
+                        "counter": counter,
+                        "po_rate": project_rate,
+                        "rate": consultant_rate,
+                        "consultant_id": consultant.id,
+                        "last_revision": revision_date,
+                        "margin": f"{margin_percentage}%",
+                        "consultant_name": consultant.name,
+                        "consultant_email": consultant.email,
+                        "marketer_name": marketer.get('name'),
+                        "marketer_email": marketer.get('email'),
+                        'vendor_name': project.submission.lead.vendor_company.name
+                    }
+                    writer.writerow([
+                        consultant_info['consultant_name'], consultant_info['marketer_name'],
+                        consultant_info['po_rate'], consultant_info['rate'], consultant_info['margin'],
+                        consultant_info['last_revision'], consultant_info['vendor_name']
+                    ])
+                    data.append(consultant_info)
+                    counter += 1
+            file.close()
+            file = open(file.name, 'r')
             if os.environ.get('ENV', 'local') == 'prod':
-                to = ['finance@consultadd.com']
+                to = ['finance@consultadd.com',  'relations@consulatdd.com', 'arun.k@consultadd.com']
+                scrum_master = User.objects.filter(
+                    team__dept='Marketing', role__name='admin', is_active=True
+                ).values_list('email', flat=True)
+                to.extend(scrum_master)
             else:
                 to = ['suman.m@consultadd.com', 'shreyas.k@consultadd.com', 'shivam.k@consultadd.com']
             mail_data = {
+                'attachments': [file.name],
                 'to': to, 'cc': [], 'bcc': [],
-                'subject': f"Consultant Rate Revision Due",
+                'subject': f"Consultant Rate Revision Due Info",
                 'template': '../templates/rate_due_notification.html',
                 'context': {
                     "data": data,
                 },
             }
             if len(data) > 0:
-                send_email(mail_data, "admin@consultadd.com")
+                send_email_attachment_multiple(mail_data, "admin@consultadd.com")
+            delete_temp_file([file.name])
         except Exception as error:
+            print(error)
             create_cron_error(job, error)
