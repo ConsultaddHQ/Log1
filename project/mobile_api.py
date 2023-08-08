e2c07cb9fb002e0387a926a535fd43ef88661e25
piyushyadav4904 <piyushyadav19629@gmail.com>
2023-08-08 15:43:49 +0530

    fixed bug

--- project/mobile_api.py
+++ project/mobile_api.py
@@ -171,7 +171,7 @@ class TimeSheetViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, Updat
                 start = datetime.strptime(start, "%Y-%m-%d").date()
                 end = request.data['end_week']
                 end = datetime.strptime(end, "%Y-%m-%d").date()
-                if week_id and week_id != 0:
+                if week_id and week_id != '0':
                     timesheet = TimeSheet.objects.filter(id=week_id,project=project,status__in=["submitted", "rejected"]).first()
                     timesheet.start = start
                     timesheet.end = end
