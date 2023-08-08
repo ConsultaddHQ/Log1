e2ca49b911e8d5dd8f9e786ba4e2258911e31fff
Shreyas Khede <88902477+shreyas-khede@users.noreply.github.com>
2023-08-08 04:49:56 -0400

    Dev (#66)
    * log1-1150 (#56)
    
    * add marketers in consultadd marketing for given team
    
    * created two new api in consultant marketing view
    
    ---------
    
    Co-authored-by: piyushyadav4904 <piyushyadav19629@gmail.com>
    
    * Revert "log1-1150 (#56)" (#63)
    
    This reverts commit a5cac4194fae8f06db96e38180714d9de2d3110c.
    
    * change the way to copy thr request data (#58)
    
    Co-authored-by: piyushyadav4904 <piyushyadav19629@gmail.com>
    
    * Log1 1146 (#57)
    
    * add screening filter in interview list
    
    * added termination filter
    
    * added other termination type on filter
    
    * change the display type status
    
    ---------
    
    Co-authored-by: Cosnultadd <shreyas.k@consultadd.com>
    Co-authored-by: piyushyadav4904 <piyushyadav19629@gmail.com>
    
    * Log1 1126 (#62)
    
    * update for paytubs on create method
    
    * changes after self code review
    
    * sent the push notificition on approved the timesheet
    
    * log1-1126
    
    * change after code review
    
    * added the completion_count
    
    ---------
    
    Co-authored-by: piyushyadav4904 <piyushyadav19629@gmail.com>
    
    ---------
    
    Co-authored-by: consultaddpiyush <135980853+consultaddpiyush@users.noreply.github.com>
    Co-authored-by: piyushyadav4904 <piyushyadav19629@gmail.com>

--- project/mobile_api.py
+++ project/mobile_api.py
@@ -134,26 +134,26 @@ class TimeSheetViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, Updat
             project.timesheet_frequency = frequency
             project.save()
             prev_timesheet = TimeSheet.objects.filter(project=project).order_by('-created')
-            if prev_timesheet:
-                if prev_timesheet.exclude(status='draft').exists():
-                    last_filled_timesheet = prev_timesheet.exclude(status='draft').first()
-                    draft_timesheet = prev_timesheet.filter(status='draft', start__gte=last_filled_timesheet.end)
-                    timesheet_start_date = draft_timesheet.last().start if draft_timesheet else last_filled_timesheet.end + timedelta(days=1)
-                    draft_timesheet.delete()
-                    self.create_timesheet(project, timesheet_start_date, frequency)
-                else:
-                    timesheet_start_date = prev_timesheet.last().start
-                    prev_timesheet.delete()
-                    self.create_timesheet(project, timesheet_start_date, frequency)
-            else:
-                self.create_timesheet(project, frequency=frequency)
+            # if prev_timesheet:
+            #     if prev_timesheet.exclude(status='draft').exists():
+            #         last_filled_timesheet = prev_timesheet.exclude(status='draft').first()
+            #         draft_timesheet = prev_timesheet.filter(status='draft', start__gte=last_filled_timesheet.end)
+            #         timesheet_start_date = draft_timesheet.last().start if draft_timesheet else last_filled_timesheet.end + timedelta(days=1)
+            #         draft_timesheet.delete()
+            #         self.create_timesheet(project, timesheet_start_date, frequency)
+            #     else:
+            #         timesheet_start_date = prev_timesheet.last().start
+            #         prev_timesheet.delete()
+            #         self.create_timesheet(project, timesheet_start_date, frequency)
+            # else:
+            #     self.create_timesheet(project, frequency=frequency)
 
             return Response({"message": "Timesheet frequency updated successfully"}, status=201)
         except Exception as error:
             write_exception(error, request)
             return Response({"error": str(error)}, status=400)
 
-    def update(self, request, *args, **kwargs):
+    def create(self, request):
         try:
             screenshot = False
             perv_attachments = request.data.get('attachments', None)
@@ -161,13 +161,44 @@ class TimeSheetViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, Updat
                 perv_attachments = list(perv_attachments.split(","))
             else:
                 perv_attachments = []
+            week_id = request.data.get('week_id', None)
             zero_hours = request.GET.get('zero_hours', None)
-            timesheet = get_object_or_404(
-                TimeSheet, id=kwargs.get('pk', None),
-                project__consultant=request.user,
-                status__in=['draft', 'rejected', 'submitted'],
-                is_active=True,
-            )
+            project_id = request.data.get('project_id', None)
+            project = get_object_or_404(Project, id=project_id)
+
+            if project.submission.work_type != "c2c":
+                start = request.data['start_week']
+                start = datetime.strptime(start, "%Y-%m-%d").date()
+                end = request.data['end_week']
+                end = datetime.strptime(end, "%Y-%m-%d").date()
+                if week_id and week_id != 0:
+                    timesheet = TimeSheet.objects.filter(id=week_id,project=project,status__in=["submitted", "rejected"]).first()
+                    timesheet.start = start
+                    timesheet.end = end
+                else:
+                    query = Q(project=project, end__gte=start, start__lte=start) | Q(project=project,end__gte=end,start__lte=end)
+                    available_timesheet = TimeSheet.objects.filter(query).order_by('-created')
+                    if available_timesheet:
+                        timesheet = available_timesheet.first()
+                        available_week = f"{timesheet.start} - {timesheet.end}"
+                        return Response({"error": f"Timesheet already exit {available_week}"}, status=400)
+
+                    timesheet = TimeSheet.objects.create(
+                        start=start, end=end, project=project, status='request',
+                    )
+                # update_timesheet = TimeSheet.objects.filter(project=project, start=start, end=end,
+                #                                           status__in=["submitted", "rejected"])
+
+            else:
+                timesheet = get_object_or_404(
+                    TimeSheet, id=request.data.get('week_id', None),
+                    project__consultant=request.user,
+                    status__in=['draft', 'rejected', 'submitted'],
+                    is_active=True,
+                )
+            if not timesheet:
+                return Response({"error": "SOMETHING WENT WRONG"}, status=400)
+
             timesheet_id = timesheet.id
             hours = float(request.data.get('hours'))
             timesheet.status = 'submitted' if timesheet.status != 'submitted' else 'updated'
@@ -187,7 +218,7 @@ class TimeSheetViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, Updat
                     attachments = Attachment.objects.filter(object_id=timesheet.id, is_active=True,
                                                             attachment_type='timesheet')
                     for attachment in attachments:
-                        if attachment.id in perv_attachments:
+                        if str(attachment.id) in perv_attachments:
                             continue
                         attachment.is_active = False
                         attachment.save()
@@ -222,9 +253,10 @@ class TimeSheetViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, Updat
 
             last_timesheet = TimeSheet.objects.filter(project=timesheet.project).aggregate(Max('end'))
             end_date = last_timesheet['end__max']
-            self.create_timesheet(
-                timesheet.project, end_date + timedelta(days=1), timesheet.project.timesheet_frequency, count=1
-            )
+            if project.submission.work_type == "c2c":
+                self.create_timesheet(
+                    timesheet.project, end_date + timedelta(days=1), timesheet.project.timesheet_frequency, count=1
+                )
             # new_ts, created = TimeSheet.objects.get_or_create(
             #     project=timesheet.project,
             #     start=end_date + timedelta(days=1),
@@ -391,7 +423,7 @@ class TimeSheetViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, Updat
             timesheet = get_object_or_404(TimeSheet, id=pk, project__consultant=request.user)
             if timesheet.status == 'rejected':
                 timesheet = TimeSheet.objects.filter(
-                    project_id=timesheet.project.id, is_active=False,
+                    project_id=timesheet.project.id, is_active=True,
                     end=timesheet.end, start=timesheet.start, status='rejected'
                 )
                 attachments = timesheet.first().attachments.filter(is_active=True)
