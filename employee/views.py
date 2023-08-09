0dd53c15897e572748a28c2243d856fb3fc08b9b
vishesh-suryavanshi <135978805+vishesh-suryavanshi@users.noreply.github.com>
2023-08-09 12:21:55 +0530

    Update views.py

--- employee/views.py
+++ employee/views.py
@@ -35,8 +35,7 @@ from log1.utils import write_exception, write_info, DONT_HAVE_ACCESS, ERROR_MSG,
 from employee.models import User, Role, Team, Asset, ResetPasswordToken, Handover, clear_expired, get_token_expiry_time, \
     DefaultCalendar, CertificateInfo, Certificate
 from employee.serializers import UserSerializer, UserSerializerLogin, EmailSerializer, PasswordTokenSerializer, \
-    AssetSerializer, UserDirectorySerializer, HandoverSerializer, UserDashboardSerializer, CertificateInfoSerializer, \
-    ChangePasswordSerializer
+    AssetSerializer, UserDirectorySerializer, HandoverSerializer, UserDashboardSerializer, CertificateInfoSerializer
 
 
 # Route - /auth/
