d02996d386e7cf7d268b876613f74463899b5939
consultaddpiyush <135980853+consultaddpiyush@users.noreply.github.com>
2023-10-02 07:08:33 -0500

    Log1 1262 (#131)
    * added the call_type
    
    * inteview bot slack card changes
    
    * changes in slack bot
    
    * call_type
    
    * delete the file
    
    * changes in bot
    
    * call_type__name
    
    * call_type__add
    
    * change the call type
    
    ---------
    
    Co-authored-by: piyushyadav4904 <piyushyadav19629@gmail.com>

--- marketing/models.py
+++ marketing/models.py
@@ -433,6 +433,7 @@ class Interview(TimeStampedModel):
     screening_type = models.CharField(_('Screening Type'), max_length=20, choices=TYPE_CHOICES)
     interview_mode = models.CharField(_('Interview Mode'), max_length=20, choices=INTERVIEW_MODE)
     status = models.CharField(_('Status'), max_length=20, choices=STATUS_CHOICES, default='scheduled')
+    call_type = models.ForeignKey(Choice, null=True, blank=True,on_delete=models.CASCADE, related_name='interview_type')
     if_previous_calendar = models.BooleanField(_('Previous Calendar'), default=True)
     failure_reason = ArrayField(models.CharField(
         _('Failure Reason'),
