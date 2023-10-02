4d321249accd7e77bed396eed0e1e7bfef06ecf4
piyushyadav4904 <piyushyadav19629@gmail.com>
2023-10-02 07:48:05 -0400

    change the call type

--- utils_app/management/commands/interview_bot.py
+++ utils_app/management/commands/interview_bot.py
@@ -93,7 +93,7 @@ class Command(BaseCommand):
                             "marketer": interview.marketer.employee_name, "position": position,
                             "consultant": interview.consultant.name, "client": interview.submission.client,
                             "ctb": f'<@{supervisor.slack_id}>' if supervisor.slack_id else supervisor.employee_name,
-                            "call_type": "otter.ai" if interview.call_type.name == "otter_al" else interview.get_call_type_display(),
+                            "call_type": "otter.ai" if interview.call_type.name == "otter_al" else interview.call_type.display_name,
                         }
                     )
             data = {
