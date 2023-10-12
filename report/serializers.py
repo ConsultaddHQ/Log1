d73f9ddce05ee384509589f7f41f9e4a0176a006
Vishesh Suryavanshi <vishesh.s@consultadd.com>
2023-10-12 15:53:30 +0530

    report serializer updated

--- report/serializers.py
+++ report/serializers.py
@@ -12,7 +12,7 @@ class ReportSerializer(serializers.Serializer):
     def get_id(self, obj):
         model = self.context.get('model')
         if obj:
-            return obj.submission.id if model in CHECK_FOR_ID else obj.id
+            return obj.submission.id if model in CHECK_FOR_ID_FIELD else obj.id
         return None
 
     def get_name(self, obj):
@@ -20,7 +20,7 @@ class ReportSerializer(serializers.Serializer):
         if obj:
             if model == 'Consultant':
                 return obj.name
-            elif model in CHECK_FOR_NAME:
+            elif model in CHECK_FOR_NAME_FIELD:
                 return obj.consultant.name if obj.consultant else None
             else:
                 return None
@@ -29,5 +29,5 @@ class ReportSerializer(serializers.Serializer):
     def get_sub_id(self, obj):
         model = self.context.get('model')
         if obj:
-            return obj.id if model in CHECK_FOR_ID else None
+            return obj.id if model in CHECK_FOR_ID_FIELD else None
         return None
