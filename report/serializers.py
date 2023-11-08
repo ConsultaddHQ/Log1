e377b07816aab7240d1003eeb236e817b48fe27a
prashantk-mu <119774946+prashantk-mu@users.noreply.github.com>
2023-11-08 13:36:10 +0530

    Revert "Log1 1308"

--- report/serializers.py
+++ report/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
 
-from employee.models import User
 from project.models import Project
 from consultant.models import Consultant
-from marketing.models import Submission, Interview, Test
+from marketing.models import Submission, Interview
 
 CHECK_FOR_NAME_FIELD = ['Submission', 'Interview', 'Project']
 CHECK_FOR_ID_FIELD = ['Interview', 'Project']
@@ -93,7 +92,7 @@ class SubmissionInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Submission
-        fields = ('id', 'consultant', 'marketer', 'client', 'vendor', 'rate', 'is_complete', 'created')
+        fields = ('id', 'consultant', 'marketer', 'client', 'vendor', 'rate',  'is_complete', 'created')
 
     @staticmethod
     def get_consultant(obj):
@@ -195,102 +194,3 @@ class ProjectInfoSerializer(serializers.ModelSerializer):
             }
         else:
             return {"name": obj.submission.consultant.name, "remote": obj.consultant.name.split(' ')[0]}
-
-
-class TimesheetProjectSerializer(serializers.ModelSerializer):
-    display_name = serializers.SerializerMethodField()
-    companies = serializers.SerializerMethodField()
-    project_type = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Project
-        fields = ('id', 'companies', 'display_name', 'project_type')
-
-    @staticmethod
-    def get_companies(obj):
-        submission = obj.submission
-        if submission:
-            return {
-                'client': submission.client,
-                'vendor': submission.lead.vendor_company.name
-            }
-        return None
-
-    @staticmethod
-    def get_display_name(obj):
-        return f'{obj.id}:{obj.submission.client}'
-
-    def get_project_type(self, obj):
-        return self.context.get('project_type')
-
-
-class TimesheetTestSerializer(serializers.ModelSerializer):
-    test_id = serializers.IntegerField(source='id')
-    companies = serializers.SerializerMethodField()
-    consultant = serializers.SerializerMethodField()
-    submit_date = serializers.SerializerMethodField()
-    assign_to = serializers.SerializerMethodField()
-    submit_month = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Test
-        fields = ('test_id', 'companies', 'consultant', 'submit_date', 'assign_to', 'submit_month')
-
-    @staticmethod
-    def get_companies(obj):
-        submission = obj.submission
-        if submission:
-            return {
-                'client': submission.client,
-                'vendor': submission.lead.vendor_company.name
-            }
-        return None
-
-    @staticmethod
-    def get_consultant(obj):
-        consultant = obj.submission.consultant if obj.submission else None
-        return {
-            'id': consultant.id,
-            'name': consultant.name
-        }
-
-    @staticmethod
-    def get_submit_date(obj):
-        if obj.submit_date:
-            return obj.submit_date.date()
-        return None
-
-    @staticmethod
-    def get_assign_to(obj):
-        engineers = obj.assign_to.all()
-        data = []
-        if engineers:
-            for engineer in engineers:
-                data.append({'employee_id': engineer.employee_id, 'name': engineer.employee_name})
-            return data
-        return None
-
-    @staticmethod
-    def get_submit_month(obj):
-        submit_date = obj.submit_date
-        if submit_date:
-            return f'{submit_date.date().year}-{str(submit_date.date().month).zfill(2)}'
-        return None
-
-
-class TimesheetUserSerializer(serializers.ModelSerializer):
-    team = serializers.SerializerMethodField()
-
-    class Meta:
-        model = User
-        fields = ('employee_name', 'email', 'team')
-
-    @staticmethod
-    def get_team(obj):
-        team = obj.team
-        if team:
-            return {
-                'name': team.name,
-                'department': team.dept
-            }
-        return None
