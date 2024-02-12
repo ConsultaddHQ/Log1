3d068bb3cdabd7123c6ae0e26bef95c68be6a163
Shreyas Khede <88902477+shreyas-khede@users.noreply.github.com>
2024-02-12 15:11:49 +0530

    Log1 1375 (#193)
    * mail functionality code enhancement
    
    * code enhancement
    
    * code enhancement
    
    * update mail service
    
    * update city filter
    
    * add vendor contact seriailizer
    
    * add vendor contact seriailizer

--- marketing/views.py
+++ marketing/views.py
@@ -29,10 +29,10 @@ from activity.serializers import ActivitySerializer
 from utils_app.mailing import send_email_without_template
 from attachment.models import Attachment, create_attachment
 from utils_app.slack_notification import MessageCard as slack
-from utils_app.thred_mail import send_email_attachment_multiple
 from notification.utils import create_notification, push_notification
 from utils_app.aws_utils import presigned_post_url, download_s3_object
 from utils_app.utils import delete_temp_file, export_to_csv, generate_s3_url, TECHNOLOGIES
+from utils_app.thred_mail import send_email_attachment_multiple, send_email_without_template
 from log1.utils import get_page_limits, post_msg_using_webhook, write_exception, write_info, DONT_HAVE_ACCESS, ERROR_MSG
 
 
@@ -3130,7 +3130,6 @@ class TestViewSets(GenericViewSet, CreateModelMixin, ListModelMixin, UpdateModel
                     {"message": "Feedback must be of more than 10 character"}, status=status.HTTP_400_BAD_REQUEST
                 )
             test.status = request.data.get('status')
-            test.submitted_by = request.user
             test.feedback = feedback
             test.save()
             assigned_test_points(test, request)
