73be42b7aec28de766a226113bd9989222fdc7f1
Cosnultadd <shreyas.k@consultadd.com>
2024-02-16 06:00:03 -0500

    bugfixes in submission detail page

--- marketing/serializers.py
+++ marketing/serializers.py
@@ -452,6 +452,7 @@ class SubmissionV2Serializer(serializers.ModelSerializer):
     @staticmethod
     def get_vendor_contact(obj):
         if obj.vendor_contact:
+            breakpoint()
             return VendorContactSerializer(obj.vendor_contact).data
         return {}
 
@@ -483,7 +484,7 @@ class SubmissionV2DetailSerializer(serializers.ModelSerializer):
     @staticmethod
     def get_vendor_contact(obj):
         if obj.vendor_contact:
-            return VendorContactSerializer(obj.vendor_contact)
+            return VendorContactSerializer(obj.vendor_contact).data
         return {}
 
     @staticmethod
