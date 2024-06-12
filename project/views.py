46d4cd1258a362976d95dc5c85b0716ed684b73d
shivamkhodre <shivam.k@consultadd.com>
2024-06-12 09:57:16 -0400

    unassign support and interview recording link changes

--- project/views.py
+++ project/views.py
@@ -1206,6 +1206,22 @@ class ProjectSupportViewSet(GenericViewSet, RetrieveModelMixin, ListModelMixin, 
     def partial_update(self, request, *args, **kwargs):
         return Response({"detail": "Method PATCH not allowed."}, status=405)
 
+    def delete(self, request, *args, **kwargs):
+        try:
+            if 'superadmin' not in request.user.roles and not ('admin' in request.user.roles and 'engineer' in request.user.roles):
+                return Response({"message": "You do not have access to perform this action"}, status=400)
+            project = get_object_or_404(Project, id=kwargs.get('project_id'))
+            supports = ProjectSupport.objects.filter(project=project)
+            count = supports.count()
+            supports.delete()
+            desc = f"{request.user.employee_name} deleted support details"
+            create_activity(project.id, 'projectsupport', request.user, desc, 'deleted')
+            return Response({"message": "Support has been deleted"}, status=status.HTTP_204_NO_CONTENT)
+        except Exception as error:
+            write_exception(error, request)
+            # Return an error response
+            return Response({"message": ERROR_MSG, "error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
+    
     @action(methods=['put'], detail=True, url_path="status")
     def status(self, request, project_id, pk):
         try:
