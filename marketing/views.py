bedfb6ab3aa295acd045c13fce77fdbb0c9782f3
shivamkhodre <shivam.k@consultadd.com>
2024-06-13 06:05:34 -0400

    code correction

--- marketing/views.py
+++ marketing/views.py
@@ -1957,6 +1957,15 @@ class InterviewViewSets(ModelViewSet):
 
             if interview_status == 'cancelled':
                 return Response({"message": "Interview can't be cancelled"}, status=400)
+            
+            interview_link = request.data.get('interviewer_link', None)
+            interview_recording_link = request.data.get('interview_recording_link', None)
+
+            if not interview_link or (interview_link and len(interview_link.strip()) == 0):
+                return Response({"message": "Invalid value of interview link, Please provide a valid link"}, status=400)
+
+            if not interview_recording_link or (interview_recording_link and len(interview_recording_link.strip()) == 0):
+                    return Response({"message": "Invalid value of interview recording link, Please provide a valid link"}, status=400)
 
             users = get_authenticated_users(request)
             queryset = Interview.objects.filter(id=kwargs.get('pk'), submission__created_by__in=users)
